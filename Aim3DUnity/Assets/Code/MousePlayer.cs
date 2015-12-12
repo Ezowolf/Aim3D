3b79dd3c7a5d4c54829aebd3d8dcdec7c99376cf
Sebastiaan <wiggler@live.nl>
2015-12-12 18:07:19 +0100

    Proof Of Concept

---
+++ Aim3DUnity/Assets/Code/MousePlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePlayer: MonoBehaviour
+{
+    private Camera camera;
+
+    public int whatToSpawnInt;
+
+    [SerializeField]
+    private GameObject[] yourObjectArray;
+
+    [SerializeField]
+    private float zAxisDistance = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = zAxisDistance;
+            Vector3 objectPos = camera.ScreenToWorldPoint(mousePos);
+            Instantiate(yourObjectArray[whatToSpawnInt], objectPos, Quaternion.identity);
+        }
+    }
+}
---
+++ Aim3DUnity/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    private Rigidbody rb;
+    private float jumpForce = 250;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+
+    }
+
+    void PlayerMove()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+
+}
