793803679aec3dd33501791a128ee352a197530e
Ezowolf <wiggler@live.nl>
2015-12-14 15:13:30 +0100

    Enemy Shooting

---
+++ Aim3DUnity/Assets/Code/EnemyShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShoot : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject bulletToFire;
+
+	private bool canIShoot = true;
+
+	public bool amIInRange = false;
+
+	[SerializeField]
+	private float shootingInterval = 1;
+
+	// Update is called once per frame
+	void Update () {
+		if(amIInRange)
+		{
+			if(canIShoot)
+			{
+				Instantiate(bulletToFire, this.transform.position, this.transform.localRotation);
+				canIShoot = false;
+				StartCoroutine(IntervalCompleter());
+			}
+
+		}
+	}
+
+	IEnumerator IntervalCompleter()
+	{
+		yield return new WaitForSeconds(shootingInterval);
+		canIShoot = true;
+	}
+}
---
+++ Aim3DUnity/Assets/Code/StopWithDistance.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopWithDistance : MonoBehaviour {
+	private GameObject transformToFollow;
+
+	private MoveForward stopThis;
+
+	private EnemyShoot shootScript;
+
+	[SerializeField]
+	private float distanceToStopAt = 3;
+
+	private float originalSpeed = 8;
+
+	// Use this for initialization
+	void Start () {
+		transformToFollow = GameObject.FindWithTag("Player");
+		stopThis = GetComponent<MoveForward>();
+		shootScript = GetComponent<EnemyShoot>();
+		originalSpeed = stopThis.moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position,transformToFollow.transform.position)<=distanceToStopAt)
+		{
+			stopThis.moveSpeed = 0;
+			shootScript.amIInRange = true;
+		}
+		else
+		{
+			stopThis.moveSpeed = originalSpeed;
+			shootScript.amIInRange = false;
+		}
+}
+}
