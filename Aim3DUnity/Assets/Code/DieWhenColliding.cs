6ffa15586ed72d13e69e6012b31341b9e3c85eb2
Ezowolf <wiggler@live.nl>
2015-12-14 11:33:48 +0100

    Destroy when colliding

---
+++ Aim3DUnity/Assets/Code/DieWhenColliding.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieWhenColliding : MonoBehaviour {
+
+	private HealthAndDie healthScript;
+
+	[SerializeField]
+	private string whatTagKillsMe;
+
+	[SerializeField]
+	private bool destroyAfterColliding;
+
+	// Use this for initialization
+	void Start () {
+		healthScript = GetComponent<HealthAndDie>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.gameObject.tag == whatTagKillsMe)
+		{
+			healthScript.health--;
+			if(destroyAfterColliding)
+			Destroy(coll.gameObject);
+		}
+	}
+}
--- Aim3DUnity/Assets/Code/HealthAndDie.cs
+++ Aim3DUnity/Assets/Code/HealthAndDie.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class HealthAndDie : MonoBehaviour {
 
-    [SerializeField]
-    private int health = 10;
+    public int health = 10;
 	// Use this for initialization
 	void Start () {
 	
