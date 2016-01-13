69d54ff007d6641832fa6d6e6fa0e654c12f4948
ezowolf <wiggler@live.nl>
2016-01-13 12:14:05 +0100

    Art #3

---
+++ Aim3DUnity/Assets/Code/ChangeToWinTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeToWinTile : MonoBehaviour {
+	[SerializeField]
+	private GameObject winTile;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "WinningPickup") 
+		{
+			Instantiate (winTile);
+			Destroy (this.gameObject);
+		}
+	}
+}
